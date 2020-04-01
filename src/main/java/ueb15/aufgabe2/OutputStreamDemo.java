5e26b0681c76074dd67ed6095cb199fe8d30868f
dstckln <daniel.stoecklein@gmail.com>
2020-04-01 23:44:02 +0200

    Initial commit

--- src/main/java/ueb15/aufgabe2/OutputStreamDemo.java
+++ src/main/java/ueb15/aufgabe2/OutputStreamDemo.java
@@ -5,12 +5,12 @@ import java.io.*;
 public class OutputStreamDemo {
 
     public void write(byte[] b, int off, int len) throws IOException {
-		if (b == null) throw new NullPointerException();
-		if (off < 0 || len < 0 || (off + len) > b.length) throw new IndexOutOfBoundsException();
-		//damit sind tatsächlich alle Fehlerfälle abgedeckt
+        if (b == null) throw new NullPointerException();
+        if (off < 0 || len < 0 || (off + len) > b.length) throw new IndexOutOfBoundsException();
+        //damit sind tatsächlich alle Fehlerfälle abgedeckt
 
-		OutputStream os = new FileOutputStream("test.txt");
-		os.write(b, off, len);
-		os.close();
+        OutputStream os = new FileOutputStream("test.txt");
+        os.write(b, off, len);
+        os.close();
     }
 }
